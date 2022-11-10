e62c2bc919ed56754f8b0c7f96840da151c496bf
thomas hunckler <th@phytodia.com>
2022-11-10 14:01:49 +0100

    create routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,20 @@ class PagesController < ApplicationController
   def home
     @newsletter = Newsletter.new
   end
+
+  def sondage
+  end
+
+  def propos
+  end
+
+  def creatrice
+  end
+
+  def facebook
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "sondages", to: "pages#sondage"
+  get "qui-sommes-nous", to: "pages#propos"
+  get "devenir-co-creatrice", to: "pages#creatrice"
+  get "facebook", to: "pages#facebook"
+  get "contact", to: "pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

cd9e85c5b118d913b12395010a4752100f5f75ff
thomas hunckler <th@phytodia.com>
2022-11-10 16:45:03 +0100

    change env prod for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,11 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
 
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "https://www.yourdomain.com" }
+  config.action_mailbox.ingress = :postmark
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
